bd79d656b222a1e0bea55de45d12bc826152a918
shen <leoshenhh@gmail.com>
2022-10-20 16:38:47 +0800

    Feat moveable

--- src/content/App.tsx
+++ src/content/App.tsx
@@ -1,6 +1,61 @@
+import { useState, useEffect, useRef } from "react";
+import './app.scss'
+
 const App = () => {
+  const [top, setTop] = useState(0);
+  const [left, setLeft] = useState(0);
+
+  const [moveable, setMoveable] = useState(false);
+
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setMouseX(e.pageX);
+    setMouseY(e.pageY);
+    setMoveable(true);
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+
+    if (moveable) {
+      const x = e.pageX - mouseX;
+      const y = e.pageY - mouseY;
+
+      setTop(top + y);
+      setLeft(left + x);
+      setMouseX(e.pageX);
+      setMouseY(e.pageY);
+    }
+  };
+  const handleMouseUp = (e: React.MouseEvent) => {
+    setMoveable(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, [moveable]);
+
+
   return (
-    <h1>123</h1>
-  )
-}
-export default App
+    <div
+      onMouseDown={(e) => handleMouseDown(e)}
+      onMouseUp={(e) => handleMouseUp(e)}
+      className="overlap"
+      style={{
+        width: "100px",
+        height: "100px",
+        position: "fixed",
+        top: top + "px",
+        left: left + "px",
+        color: "white",
+      }}
+    >
+      <div>{mouseX}</div>
+      <div>{mouseY}</div>
+    </div>
+  );
+};
+export default App;
--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,9 +1,14 @@
-import '/src/tailwind.css'
+import "/src/tailwind.css";
 const App = () => {
   return (
-    <h1 className="bg-black text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
-}
-export default App
+    <div className="h-40 w-36 p-4 shadow-2xl">
+      <button
+        type="button"
+        className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        添加mask
+      </button>
+    </div>
+  );
+};
+export default App;
